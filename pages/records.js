4011d7c10e430a7d48e4a43b0e3fe773f35d051b
nigelmarongere <nigelmarongere@gmail.com>
2023-01-13 11:43:37 +0000

    progress made

--- pages/records.js
+++ pages/records.js
@@ -17,6 +17,41 @@ export default function Records() {
     const [date, setDate] = useState(today);
 
     // workers
+    const [workers, setWorkers] = useState(['Alex', 'Gianluca', 'Nigel']);
+    const listWorkers = workers.map((worker) =>
+        <option key={worker} value={worker}>{worker}</option>
+    );
+
+    const [hours, setHours] = useState([4, 8, 16]);
+    const listHours = hours.map((hour) =>
+        <option key={hour} value={hour}>{hour}</option>
+    );
+
+    const [workerRecords, setWorkerRecords] = useState([{date_recorded: date, worker_name: 'Joe', hours_worked: 8, additional_info: 'Arrived 15 minutes late'}]);
+    const listWorkerRecords = workerRecords.map((workerRecord) =>
+        <li key={current.getTime()}>{`
+            ${workerRecord.worker_name} worked 
+            ${workerRecord.hours_worked} hours. 
+            ${workerRecord.additional_info}
+        .`}</li>
+    );
+
+    const [name, setName] = useState('');
+    const [time, setTime] = useState(0);
+    const [note, setNote] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setWorkerRecords([
+            ...workerRecords,
+            {date_recorded: date, worker_name: name, hours_worked: time, additional_info: note}
+        ]);
+        console.log(workerRecords);
+        setName('');
+        setTime(0);
+        setNote('');
+
+    }
 
     // bookkeeping
     
@@ -27,12 +62,30 @@ export default function Records() {
                 <h3>Date</h3>
                 <input 
                     type="date"
-                    value={date} 
+                    value={date}
                     onChange={(event) => setDate(event.target.value)}
                 />
             </div>
             <div>
                 <h3>Workers</h3>
+                <ul>
+                    {listWorkerRecords}
+                </ul>
+                <form onSubmit={handleSubmit}>
+                    <label>Name </label>
+                    <select onChange={(event) => setName(event.target.value)}>
+                        <option value="">--worker--</option>
+                        {listWorkers}
+                    </select>
+                    <label> Hours </label>
+                    <select onChange={(event) => setTime(event.target.value)}>
+                        <option value="">--hours--</option>
+                        {listHours}
+                    </select>
+                    <label>Notes</label>
+                    <input type="text" placeholder="--note--" onChange={(event) => setNote(event.target.value)}></input>
+                    <button type="submit">Add</button>
+                </form>
             </div>
             <div>
                 <h3>bookkeeping</h3>
